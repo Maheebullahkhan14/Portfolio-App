88c25150aa0383bfd05f366d21dd879c9a8bd16b
Khan Maheebullah Nisar Ahmed <107109005+Maheebullahkhan14@users.noreply.github.com>
2023-01-07 07:32:57 -0800

    Add files via upload

---
+++ Portfolio/src/pages/Resume.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <>
+      <div className="Resume-Content">
+        <h2>Resume</h2>
+
+        <div className='Downloadbtn'>Download Resume</div>
+
+        <iframe src="https://drive.google.com/file/d/1I1h31t-jZXIYAEFxezrjeqyUsI7WAm3F/preview" width="800" height="1130" allow="autoplay"></iframe>
+
+       
+      </div>
+    </>
+  );
+};
+
+export default Resume;
